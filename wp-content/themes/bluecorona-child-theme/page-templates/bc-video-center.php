510cde1509b9e4fefb60b5d90f982ace0042dcbd
Nikhil <nikhil.verma@twinspark.co>
2020-03-30 20:55:57 +0530

    video page

--- wp-content/themes/bluecorona-child-theme/page-templates/bc-video-center.php
+++ wp-content/themes/bluecorona-child-theme/page-templates/bc-video-center.php
@@ -20,7 +20,13 @@ get_header();
 	.prev-btn a:hover, .next-btn a:hover{
 		background-color: #005076;
 	}
+
 	.content-style ul li::before{top: 1em;}
+	.video-system.v1 li [class*="pad-height-"] .btn-style{
+    position: absolute;
+    bottom: 1em;
+    right: 1em;
+}
 </style>
 <section class="bread-crumbs v1 thin bg-box-none light-bg" id="BreadCrumbsV1Thin">
 	
@@ -56,9 +62,14 @@ if ( $parent->have_posts() ) : ?>
 <ul>
 			<li>
 				<div class="divider flex-middle top-margin-small bottom-margin-small">
-					<strong class="title-style-3 title-color-3">
+					<a href="<?php the_permalink(); ?>" title="Sherlock Heating &amp; Air Conditioning">
+						<strong class="title-style-3">
+							Sherlock Heating &amp; Air Conditioning
+						</strong>
+					</a>
+					<!-- <strong class="title-style-3 title-color-3">
 						<?php the_title(); ?>
-					</strong>
+					</strong> -->
 					<span class="line auto"></span>
 <a href="<?php the_permalink(); ?>" class="btn-colors btn-style fit" title="<?php the_title(); ?>"><?php echo "(".count( $children ).")  Videos"; ?>
 					</a>
@@ -79,7 +90,32 @@ if ( $parent->have_posts() ) : ?>
 				if ( $parent->have_posts() ) : ?>
 					<ul class="video-list flex-wrap-grid-large-block-800">
 					<?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
-						<li class="third relative featured" data-item="i" data-key="109328">
+					
+					<li class="third relative featured" data-item="i" data-key="109328">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<div class="img pad-height-50 bg-box unlike-bg border-radius-item" role="presentation">
+
+					<?php if(get_field('video_thumbnail')){ 
+						$vthumb = get_field('video_thumbnail');
+						?>
+						<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="<?php echo $vthumb['alt']; ?>" title="<?php the_title(); ?>" style="background-image:url('<?php echo $vthumb['url']; ?>')">
+					<?php } else { ?>
+					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Meet The Owner" title="Meet The Owner" style="background-image:url('<?php echo get_stylesheet_directory_uri()?>/images/default-video.png')">
+					<?php } ?>
+
+					
+					
+					<span class="btn-style btn-colors flex-middle-center-inline">
+					<svg viewBox="0 0 36 36" title="Play" data-use="<?php echo get_stylesheet_directory_uri();?>/cms/svg/site/5d1zpk6tjes.36.svg#play">
+					<path d="M18 1.5A16.5 16.5-3803.788 1 1 1.5 18A16.52 16.52-3803.788 0 1 18 1.5Zm0-1.5a18 18 0 1 0 18 18A18 18-3803.788 0 0 18 0Zm-5.244 26.542a0.784 0.784 0 0 1-0.384-0.1a0.755 0.755 0 0 1-0.372-0.649V10.2a0.75 0.75 0 0 1 1.128-0.65l13.5 7.8a0.754 0.754 0 0 1 0 1.3l-13.5 7.793a0.752 0.752 0 0 1-0.384 0.104h0.012Zm0.744-15.04v12.99l11.244-6.496l-11.244-6.496h0Z"></path>
+					</svg>
+					</span>
+					</div>
+					<strong class="title-style-4 top-margin-tiny text-center"><?php the_title(); ?></strong>
+					</a>
+					</li>
+
+						<?php /*<li class="third relative featured" data-item="i" data-key="109328">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 							<div class="img pad-height-50 bg-box unlike-bg border-radius-item" role="presentation">	
 							<?php if(get_field('video_thumbnail')){ 
@@ -87,18 +123,19 @@ if ( $parent->have_posts() ) : ?>
 							?>
 								<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="<?php echo $vthumb['alt']; ?>" title="<?php the_title(); ?>" style="background-image:url('<?php echo $vthumb['url']; ?>')">
 							<?php } else { ?>
-							<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Meet The Owner" title="Meet The Owner" style="background-image:url('/wp-content/uploads/2020/03/03-Downstown_20171013011841-1-1.png')">
+							<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Meet The Owner" title="Meet The Owner" style="background-image:url('https://www.sherlockair.com/cms/thumbnails/00/501x285/images/video-thumbnails/video.jpg')">
 							<?php } ?>
-							
+							<!-- http://sherlockheatac.wpengine.com/wp-content/uploads/2020/03/03-Downstown_20171013011841-1-1.png -->
+							<!-- https://www.sherlockair.com/cms/thumbnails/00/501x285/images/video-thumbnails/video.jpg -->
 							<span class="btn-style btn-colors flex-middle-center-inline">
-								<svg viewBox="0 0 36 36" title="Play"><use data-href="/cms/svg/site/5d1zpk6tjes.36.svg#play"></use></svg>
+								<svg viewBox="0 0 36 36" title="Play"><use data-href="https://www.sherlockair.com/cms/svg/site/5d1zpk6tjes.36.svg#play"></use></svg>
 							</span>
 									
 								
 							</div>
 							<strong class="title-style-4 top-margin-tiny text-center"><?php the_title(); ?></strong>
 						</a>
-					</li>
+					</li> */?>
 					<?php endwhile; ?>
 					</ul>
 				<?php endif; //wp_reset_postdata(); ?>
