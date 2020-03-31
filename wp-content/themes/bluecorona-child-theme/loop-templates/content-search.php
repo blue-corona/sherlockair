bcc29c04b3ae4469245c9b2460f09fb097a60231
nikhil <nikhil.verma@twinspark.co>
2020-03-31 09:54:02 -0400

    search result permalink update

--- wp-content/themes/bluecorona-child-theme/loop-templates/content-search.php
+++ wp-content/themes/bluecorona-child-theme/loop-templates/content-search.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <li class="flex- third auto half" data-item="i">
-	<a class="flex-column-top bg-box full scaling-item border-radius-item" href="<?php get_the_permalink();?>">
+	<a class="flex-column-top bg-box full scaling-item border-radius-item" href="<?php echo get_the_permalink();?>">
 		<div class="auto full side-padding-large vertical-padding-tiny flex-column-top">
 			<div class="auto full">
 				<strong class="title-style-2 title-color-2"><?php the_title();?></strong>
