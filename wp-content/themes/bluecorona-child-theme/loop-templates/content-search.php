e83eb6f920104b1a23b269a7c9a2e5df7cb104d3
Nikhil <nikhil.verma@twinspark.co>
2020-03-31 15:09:16 +0530

    update style on search page

--- wp-content/themes/bluecorona-child-theme/loop-templates/content-search.php
+++ wp-content/themes/bluecorona-child-theme/loop-templates/content-search.php
@@ -14,9 +14,12 @@ defined( 'ABSPATH' ) || exit;
 		<div class="auto full side-padding-large vertical-padding-tiny flex-column-top">
 			<div class="auto full">
 				<strong class="title-style-2 title-color-2"><?php the_title();?></strong>
-				<p class="no-bottom-margin"><?php the_excerpt(); ?> ...</p>
+				<p class="no-bottom-margin"><?php the_excerpt(); ?></p>
 			</div>
 			<span class="btn v2 fit top-margin-tiny">Visit this page</span>
 		</div>
 	</a>
 </li>
+<style type="text/css">
+a:hover {text-decoration: none;}
+</style>
