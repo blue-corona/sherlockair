20be18e6199ea65260735078ca65eb2dbc0465e0
Greg Martin <gmartin@bluecorona.com>
2020-04-11 14:53:55 -0400

    Play button on videos, border fix

--- wp-content/themes/bluecorona-child-theme/page-templates/bc-video-center.php
+++ wp-content/themes/bluecorona-child-theme/page-templates/bc-video-center.php
@@ -99,7 +99,7 @@ if ( $parent->have_posts() ) : ?>
 					
 					<li class="third relative featured" data-item="i" data-key="109328">
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<div class="img pad-height-50 bg-box unlike-bg border-radius-item" role="presentation">
+					<div class="img pad-height-50 bg-box unlike-bg" role="presentation">
 
 					<?php if(get_field('video_thumbnail')){ 
 						$vthumb = get_field('video_thumbnail');
@@ -112,9 +112,9 @@ if ( $parent->have_posts() ) : ?>
 					
 					
 					<span class="btn-style btn-colors flex-middle-center-inline">
-					<svg viewBox="0 0 36 36" title="Play" data-use="<?php echo get_stylesheet_directory_uri();?>/cms/svg/site/5d1zpk6tjes.36.svg#play">
-					<path d="M18 1.5A16.5 16.5-3803.788 1 1 1.5 18A16.52 16.52-3803.788 0 1 18 1.5Zm0-1.5a18 18 0 1 0 18 18A18 18-3803.788 0 0 18 0Zm-5.244 26.542a0.784 0.784 0 0 1-0.384-0.1a0.755 0.755 0 0 1-0.372-0.649V10.2a0.75 0.75 0 0 1 1.128-0.65l13.5 7.8a0.754 0.754 0 0 1 0 1.3l-13.5 7.793a0.752 0.752 0 0 1-0.384 0.104h0.012Zm0.744-15.04v12.99l11.244-6.496l-11.244-6.496h0Z"></path>
-					</svg>
+						<svg viewBox="0 0 36 36" title="Play" data-use="<?php echo get_stylesheet_directory_uri();?>/cms/svg/site/5d1zpk6tjes.36.svg#play">
+						<path d="M18 1.5A16.5 16.5-3803.788 1 1 1.5 18A16.52 16.52-3803.788 0 1 18 1.5Zm0-1.5a18 18 0 1 0 18 18A18 18-3803.788 0 0 18 0Zm-5.244 26.542a0.784 0.784 0 0 1-0.384-0.1a0.755 0.755 0 0 1-0.372-0.649V10.2a0.75 0.75 0 0 1 1.128-0.65l13.5 7.8a0.754 0.754 0 0 1 0 1.3l-13.5 7.793a0.752 0.752 0 0 1-0.384 0.104h0.012Zm0.744-15.04v12.99l11.244-6.496l-11.244-6.496h0Z"></path>
+						</svg>
 					</span>
 					</div>
 					<strong class="title-style-4 top-margin-tiny text-center"><?php the_title(); ?></strong>
